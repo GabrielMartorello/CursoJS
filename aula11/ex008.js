0ad062730162a8e0612cc937b580db0bcbae7ee9
Gabriel Martorello <gabrielmartorello@outlook.com>
2022-07-15 14:48:24 -0300

    aula 10 ate 12

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log (`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade maxima. MULTADO`)
+}
+console.log(`Use sinto`)
